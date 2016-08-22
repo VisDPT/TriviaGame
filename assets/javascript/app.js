519c8ad96635fcc04bf7f157e509171e43b7046f
Dr. Visalam Mahalingam PT <Jyo@Visalams-MacBook-Air.local>
2016-08-22 15:13:08 -0500

    V2

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+/*You'll create a trivia game that shows only one question until the player answers it or their time runs out.
+
+If the player selects the correct answer, show a screen congratulating them for choosing the right option. After a few seconds, display the next question -- do this without user input.
+
+The scenario is similar for wrong answers and time-outs.
+
+If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
+If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
+On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page). */
+
+
+// Create html skeleton
+		// Instruct the user how to play
+
+	// Setup the game
+		// Start Button starts game
+		// Screen Displays 
+				//time remaining for question
+				//multiple choice answers
+				//hover effects and click over answers
+
+		//user clicks answer
+			//display for right answer
+			//display for wrong answer
+		//RESET
+			// to new question after few secs
+			//seconds reset
+			//and display new question
+		//at end of game, displays:
+			// timer stops
+			// Correct Answers
+			//incorrect answers
+			//unanswered
+			//START OVER BUTTON - that does not reload page
